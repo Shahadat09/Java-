21e539379134430107af70919c3a27867d2beb0c
Shahadat09 <107842646+Shahadat09@users.noreply.github.com>
2023-06-25 15:52:21 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,19 +1,21 @@
 import java.util.*;
-public class Main {
-    public static void update(int marks[]){
-        for(int i=0;i<marks.length;i++){
-            marks[i] =marks[i]+1;
-        }
-
-    }
+public class Main{
     public static void main(String []args){
-
-        int marks[]={97,98,79};
-        update(marks);
-        System.out.println(marks[0]);
-        for(int i=0;i<marks.length;i++){
-            System.out.println(marks[i]);
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+         int Numbers[]=new int[size];
+        for(int i=0;i<size;i++){
+            Numbers[i]= sc.nextInt();
         }
 
+        int x=sc.nextInt();
+        for(int i=0;i<size;i++){
+
+            if(Numbers[i]==x){
+                System.out.println("X found at index"+ i);
+            }
+
+
+        }
     }
 }
